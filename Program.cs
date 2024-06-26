cbc6063393f15e3ff67143b1bda62bc824bdd2b3
asplicity <ticobeier@gmail.com>
2024-06-26 21:38:06 +0200

    OOP now gud

--- Program.cs
+++ Program.cs
@@ -1,16 +1,33 @@
-﻿internal class Program {
-    class Card(string Letter, string number) {
-        string letter = Letter;
-        string number = number;
-    }
-    class Deck(Card[] Deck){
-        Card[] deck = Deck;
+﻿using System;
+using System.Collections.Generic;
 
-        public void shuffle() {
+internal class Program {
+    class Card {
+        public string Letter { get; }
+        public string Number { get; }
+
+        public Card(string letter, string number) {
+            Letter = letter;
+            Number = number;
+        }
+        
+    }
+
+    class Deck {
+        private List<Card> deck;
+
+        public Deck(List<Card> cards) {
+            deck = new List<Card>(cards);
+        }
+
+        public void Shuffle() {
             Random rnd = new Random();
-            int l = deck.Length;
-            for(int i = 0; i < l; i++) {
-                
+            int l = deck.Count;
+            for (int i = 0; i < l; i++) {
+                int j = rnd.Next(0, deck.Count);
+                Card temp = deck[j];
+                deck.RemoveAt(j);
+                deck.Add(temp);
             }
         }
     }
