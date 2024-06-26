85e33d2cb8c7207dad0295a67153ff4f65ea695b
asplicity <ticobeier@gmail.com>
2024-06-26 15:10:19 +0200

    a

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@
     {
         string Stapel = "A1A2A3A4B1B2B3B4C1C2C3C4D1D2D3D4E1E2E3E4F1F2F3F4G1G2G3G4H1H2H3H4";
         int KartenImBlatt = Stapel.Length / 2;
+        var StapelPlayer = "";
+        var StapelBot = "";
         Random rnd = new Random();
         string zwischen = "";
         for (int i = 0; i < 32; i++) {
@@ -14,6 +16,16 @@
             zwischen += Karte;
         }
         Stapel = zwischen;
-        Console.WriteLine(Stapel);
+        for (int i = 0; i < 13; i++) {
+            var Karte = Stapel.Substring(0, 2);
+            if (i % 2 == 0) {
+                StapelPlayer += Karte;
+            } else{
+                StapelBot += Karte;
+            }
+            Stapel = Stapel.Replace(Karte, "");
+        }
+
+
     }
 }
