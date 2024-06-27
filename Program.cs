51ae09b6dbba073991f98c0e9e7e594611e6f7e8
asplicity <ticobeier@gmail.com>
2024-06-27 16:20:22 +0200

    Main funktion und mehr

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 internal class Program {
     class Card {
@@ -10,7 +11,15 @@ internal class Program {
             Letter = letter;
             Number = number;
         }
-        
+        public int to_num() {
+            string alph = "ABCDEFGH";
+            return int.Parse(alph.IndexOf(Letter) + "" + Number);
+        }
+        public override string ToString()
+        {
+            return Letter + Number;
+        }
+
     }
 
     class Deck {
@@ -30,5 +39,63 @@ internal class Program {
                 deck.Add(temp);
             }
         }
+        public void add(Card card) {
+            deck.Add(card);
+        }
+        public void remove(Card card) {
+            deck.Remove(card);
+        }
+        public Card Draw() {
+            return deck[0];
+        }
+
+
+        public void sortDeck() {
+            int n = deck.Count();
+            int i, j;
+            Card temp;
+            bool swapped;
+            for (i = 0; i < n - 1; i++) {
+                swapped = false;
+                for (j = 0; j < n - i - 1; j++) {
+                    if (deck[j].to_num() > deck[j + 1].to_num()) {
+                        temp = deck[j];
+                        deck[j] = deck[j + 1];
+                        deck[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (swapped == false) {
+                    break;
+                }
+            }
+        }
+
+        public override string ToString() {
+            string output = "";
+            foreach (Card card in deck) {
+                output += card.ToString() + ", ";
+            }
+            return output;
+        }
+    }
+
+    static void Main(string[] args) {
+        var alph = "ABCDDEFGH";
+        var num = "1234";
+        Deck deck = new Deck(new List<Card>());
+        foreach(var i in alph) {
+            foreach(var j in num) {
+                Card card = new Card(i.ToString(), j.ToString());
+                deck.add(card);
+
+            }
+        }
+
+        deck.Shuffle();
+        Console.WriteLine(deck.ToString());
+        deck.sortDeck();
+        Console.WriteLine(deck.ToString());
     }
 }
